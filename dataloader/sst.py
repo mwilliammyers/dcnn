1067380fd07f31f5842a49be5bb0c2adbca32404
William Myers <mwilliammyers@gmail.com>
2018-03-22 18:32:49 -0600

    Change dataset and word vector directories

--- dataloader/sst.py
+++ dataloader/sst.py
@@ -7,16 +7,20 @@ def load():
     text = data.Field()
     label = data.Field(sequential=False)
 
+    data_root = '/mnt/pccfs/not_backed_up/data'
+
+    sst_root = data_root + '/stanford_sentiment_treebank'
     train, val, test = datasets.SST.splits(
         text,
         label,
+        root=sst_root,
         fine_grained=True,
         train_subtrees=True,
         filter_pred=lambda ex: ex.label != 'neutral')
 
-    f = torchtext.vocab.FastText()
+    f = torchtext.vocab.FastText(cache=data_root + '/fasttext')
     text.build_vocab(train, vectors=f)
     text.vocab.extend(f)
     label.build_vocab(train)
 
-    return datasets.SST.iters(batch_size=4)
+    return datasets.SST.iters(batch_size=4, root=sst_root)
