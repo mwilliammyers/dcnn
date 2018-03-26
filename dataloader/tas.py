8ab41ee146b008b9f02ab1159b0ecda29be347ec
William Myers <mwilliammyers@gmail.com>
2018-03-26 12:53:00 -0600

    Refactor tas into load() function

--- dataloader/tas.py
+++ dataloader/tas.py
@@ -1,19 +1,36 @@
+import torch
 from torchtext import data as ttd
-import process_tweets as pt
+import dataloader.process_tweets as pt
 import nltk
 
-tokenizer = nltk.tokenize.TweetTokenizer()
-text = ttd.Field(
-	tokenize=tokenizer.tokenize, 
-	preprocessing=pt.preprocess(), 
-	lower=True, 
-	batch_first=True)
-label = ttd.Field(sequential=False, batch_first=True)
 
-fields = {'airline_sentiment': ('label',label), 'text': ('text', text)}
-dset = ttd.TabularDataset('../data/twitter_airlines.csv', 'csv', fields)
+def load(batch_size=4, device=0, embedding_length=60):
+    """
+    Load the twitter airline sentiment dataset.
 
-text.build_vocab(dset)
-label.build_vocab(dset)
+    Arguments:
+        batch_size: Batch_size
+        device: Device to create batches on. Use - 1 for CPU and None for
+            the currently active GPU device.
+    """
+    tokenizer = nltk.tokenize.TweetTokenizer()
+    text = ttd.Field(
+        tokenize=tokenizer.tokenize,
+        preprocessing=pt.preprocess(),
+        lower=True,
+        batch_first=True)
+    label = ttd.Field(sequential=False, batch_first=True)
 
-print (text.vocab.freqs)
+    fields = {'airline_sentiment': ('label', label), 'text': ('text', text)}
+    dset = ttd.TabularDataset('data/twitter_airlines.csv', 'csv', fields)
+
+    text.build_vocab(dset)
+    label.build_vocab(dset)
+
+    embed_vector = torch.rand(len(text.vocab), embedding_length)
+    text.vocab.set_vectors(text.vocab.stoi, embed_vector, embedding_length)
+
+    train, test, val = dset.split([.6, .2, .2])
+
+    return ttd.BucketIterator.splits(
+        (train, val, test), batch_size=batch_size, device=device)
