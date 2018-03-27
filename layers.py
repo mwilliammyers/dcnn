3865c33323277b829421741e4e551549582409bd
William Myers <mwilliammyers@gmail.com>
2018-03-27 16:33:38 -0600

    Create layers.py
    [WIP]

---
+++ layers.py
@@ -0,0 +1,10 @@
+import torch
+
+class Fold(torch.nn.Module):
+    def __init__(self, num=2, axis=0):
+        self.num = num
+        self.axis = axis
+        super(Fold, self).__init__()
+
+    def forward(self, input):
+        return input
