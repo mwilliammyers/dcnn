41401c74db849a4c0feb4069377b72a6a3f54523
Connor Anderson <thecatalystak@gmail.com>
2018-03-28 12:22:46 -0600

    Wrote the folding layer and conversion back and forth between channel representation of the network activations.

--- dcnn.py
+++ dcnn.py
@@ -1,29 +1,67 @@
 import dataloader.tas as dataloader
 import torch
+import layers
 import tqdm
 
 
 class Model(torch.nn.Module):
     def __init__(self, num_embeddings, embedding_dim):
         super(Model, self).__init__()
+
+        self.num_filters = [6, 14]
+        self.kernel_size = [7, 5]
+
         self.embedding = torch.nn.Embedding(num_embeddings, embedding_dim)
-        self.conv1 = torch.nn.Conv1d(embedding_dim, 6 * embedding_dim, kernel_size=7, padding=6, groups=embedding_dim)
+        self.conv1 = torch.nn.Conv1d(
+            in_channels = embedding_dim, 
+            out_channels = self.num_filters[0] * embedding_dim, 
+            kernel_size = self.kernel_size[0],
+            padding = self.kernel_size[0]-1, 
+            groups = embedding_dim)
+        self.fold1 = layers.fold(2, 2)
 
     def forward(self, x):
         x = self.embedding(x)
         x = x.permute(0, 2, 1)
         x = self.conv1(x)
+
+        x = self._to_channel_view(x, self.num_filters[0])
+        x = self.fold1(x)
+        x = self._from_channel_view(x)
+
         return x
+
+    def _to_channel_view(self, x, channels):
+        # Get a 4D (batch, channels, embed, length) view of the 3D 
+        # (batch, embed*channels, length) Tensor. Note that axis 1 of the input
+        # has the form [row1-filter1, row1-filter2, ..., rowd-filterk] where d
+        # is the embedding length and k is the number of conv filters. We want
+        # each channel to represent all the input rows, so we need to collate
+        # appropriately
+        (b, d, s), k = x.size(), channels
+        x = x.view(b, d // k, k, s).permute(0,2,1,3)
+        return x
+
+    def _from_channel_view(self, x):
+        # Get a 3D (batch, embed*channels, length) view of the 4D
+        # (batch, channels, embed, length) Tensor. See self._to_channel_view
+        # for more info.
+        b, k, d, s = x.size()
+        x = x.permute(0,2,1,3).contiguous().view(b, d * k, s)
+        return x        
 
 
 if __name__ == '__main__':
     embedding_dim = 60
     batch_size = 4
 
-    train_iter, val_iter, test_iter = dataloader.load(embedding_dim, batch_size)
+    device = None if torch.cuda.is_available() else -1
+    train_iter, val_iter, test_iter = dataloader.load(embedding_dim, batch_size, device=device)
     num_embeddings = len(train_iter.dataset.fields['text'].vocab)
 
-    model = Model(num_embeddings, embedding_dim).cuda()
+    model = Model(num_embeddings, embedding_dim)
+    if torch.cuda.is_available():
+        model = model.cuda()
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
--- layers.py
+++ layers.py
@@ -1,11 +1,46 @@
 import torch
 
 
-class Fold(torch.nn.Module):
-    def __init__(self, num=2, axis=0):
-        self.num = num
-        self.axis = axis
-        super(Fold, self).__init__()
+class fold(torch.nn.Module):
+    r'''Folds an input Tensor along an axis by a folding factor.
+    Expects a 4D Tensor of shape (B, C, R, C). The output will be a 4D
+    Tensor with the size of the folding axis reduced by the folding
+    factor.
 
-    def forward(self, input):
-        return input
+    The folding operation collapses the Tensor along the given axis by
+    adding groups of consecutive planes together. If axis=2 and factor=2,
+    the output would have shape (B, C, R/2, C).
+
+    The size of the folding axis should be an integer multiple of the
+    folding factor.
+    '''
+
+    def __init__(self, factor=2, axis=2):
+        super(fold, self).__init__()
+
+        self.factor = factor
+        self.axis = axis
+
+        slices = [slice(None) for _ in range(4)]
+        slices[self.axis] = slice(None, None, 2)
+        self.slices1 = tuple(slices)
+        slices[self.axis] = slice(1, None, 2)
+        self.slices2 = tuple(slices)
+
+    def forward(self, x):
+
+        dim = x.dim()
+        if dim != 4:
+            raise ValueError(f'Expected Tensor of dimension 4, '
+                'got Tensor of dimension {dim}')
+        if not self.axis < dim:
+            raise IndexError(f'Axis {self.axis} outside the range for Tensor '
+                'with dimension {dim}'.format(self.axis, dim))
+
+        if self.factor == 2:
+            x = x[self.slices1] + x[self.slices2]
+        else:
+            for _ in range(self.factor-1):
+                x = x[self.slices1] + x[self.slices2]
+
+        return x
