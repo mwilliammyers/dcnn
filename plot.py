17aaf6c82e2afdfd5b965e8298d47057f6a268df
Connor <catalys1@gmail.com>
2018-04-05 13:13:43 -0600

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,22 +3,36 @@ import numpy as np
 import struct
 import os
 
-base = './logs'
-fp = base + '/stats'
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-f',
+    dest='fp',
+    metavar='FILE',
+    default='logs/stats',
+    help='Path to binary file containing data')
+args = parser.parse_args()
+
+fp = args.fp
 data = open(fp, 'rb').read()
 data = struct.unpack('f' * (len(data) // 4), data)
 data = np.array(data).reshape(-1, 4)
-
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-ax1.plot(data[:,0], label='train')
-ax1.plot(data[:,2], label='val')
+ax1.plot(data[:,0], color='b', label='train')
+ax1.plot(data[:,2], color='g', label='val')
 ax1.legend()
 ax1.set_title('Loss')
 ax1.set_ylabel('Loss (cross entropy)')
 
-ax2.plot(data[:,1] * 100, label='train')
-ax2.plot(data[:,3] * 100, label='val')
-ax2.legend()
+data[:,[1,3]] *= 100
+ax2.plot(data[:,1], color='b', label='train')
+ax2.plot(data[:,3], color='g', label='val')
+ax2.hlines(
+    (data[:,1].max(), data[:,3].max()),
+    0, len(data)-1,
+    colors=('b', 'g'),
+    linestyle='dotted')
+ax2.legend(loc='lower right')
 ax2.set_title('Accuracy')
 ax2.set_xlabel('Batches')
 ax2.set_ylabel('Accuracy')
