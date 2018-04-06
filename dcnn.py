601b5856f2001b014bd75439086d6c6f13227c3c
Connor <thecatalystak@gmail.com>
2018-04-05 22:01:49 -0600

    Add CLI options to make more configurable

--- dcnn.py
+++ dcnn.py
@@ -18,14 +18,14 @@ def get_arguments():
         metavar='EPOCHS',
         type=int,
         default=5,
-        help='Number of epochs to train for.')  # yapf: disable
+        help='Number of epochs to train for')  # yapf: disable
     parser.add_argument(
         '--batch-size',
         dest='batch_size',
         metavar='BATCH-SIZE',
         type=int,
         default=4,
-        help='Size of a mini batch.')  # yapf: disable
+        help='Size of a mini batch')  # yapf: disable
     parser.add_argument(
         '--log',
         dest='log',
@@ -41,9 +41,29 @@ def get_arguments():
         default='dcnn',
         choices=model_choices,
         help=f'Model to use. One of {model_choices}')  # yapf: disable
+    parser.add_argument(
+        '--lr',
+        dest='learning_rate',
+        metavar='LEARNING-RATE',
+        type=float,
+        default=0.1,
+        help='Learning rate')  # yapf: disable
+    parser.add_argument(
+        '--embed-dim',
+        dest='embedding_dim',
+        metavar='EMBED-DIMENSION',
+        type=int,
+        default=60,
+        help='Dimension of the word embeddings')  # yapf: disable
+    parser.add_argument(
+        '--eval-period',
+        dest='eval_period',
+        metavar='NUM-BATCHES',
+        type=int,
+        default=200,
+        help='Number of training batches between validation evals')  # yapf: disable
 
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 def calc_accuracy(outputs, targets):
     correct = (outputs.data.max(dim=1)[1] == targets.data)
@@ -54,7 +74,9 @@ if __name__ == '__main__':
     args = get_arguments()
     num_epochs = args.epochs
     batch_size = args.batch_size
-    embedding_dim = 60
+    lr = args.learning_rate
+    embedding_dim = args.embedding_dim
+
     device = None if torch.cuda.is_available() else -1  # None == GPU, -1 == CPU
     load_data = dataloader.twitter(embedding_dim=embedding_dim, batch_size=batch_size, device=device)
 
@@ -64,9 +86,11 @@ if __name__ == '__main__':
     num_classes = len(train_iter.dataset.fields['label'].vocab)
 
     if args.model == 'dcnn':
+        # create a dynamic cnn model
         model = models.Model(num_embeddings, embedding_dim, num_classes)
         print('Running model DCNN')
     elif args.model == 'mlp':
+        # create an mlp model to compare against
         max_length = max(len(x.text) for x in train_iter.data())
         model = models.MLP(num_embeddings, embedding_dim, max_length, num_classes)
         print('Running model MLP')
@@ -74,15 +98,15 @@ if __name__ == '__main__':
         model = model.cuda()
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adagrad(model.params(), lr=.1)
+    optimizer = torch.optim.Adagrad(model.params(), lr=lr)
 
     log = logger.Logger(args.log)
 
-    # stats stores [train_loss, train_acc, test_loss, test_acc]
+    # `stats` has form [train_loss, train_acc, test_loss, test_acc]
     stats = np.zeros(4, dtype='float64')
-    update_period = 200
+    eval_period = args.eval_period
     with tqdm.tqdm(train_iter, total=len(train_iter) * num_epochs) as progress:
-        stats[:2] = 0
+        stats[:] = 0
         for i, batch in enumerate(train_iter):
             optimizer.zero_grad()
 
@@ -95,14 +119,13 @@ if __name__ == '__main__':
             stats[1] += calc_accuracy(outputs, batch.label)
 
             progress.update()
-            if i % update_period == update_period - 1:
-                stats[2:] = 0
-                for j, batch in enumerate(val_iter):
+            if (i % eval_period) == (eval_period - 1):
+                for batch in val_iter:
                     outputs = model(batch.text)
                     loss = criterion(outputs, batch.label)
                     stats[2] += loss.data[0]
                     stats[3] += calc_accuracy(outputs, batch.label)
-                stats[:2] /= update_period
+                stats[:2] /= eval_period
                 stats[2:] /= len(val_iter)
                 log.log(stats.astype('float32'))
                 progress.set_postfix(val_loss=stats[2], train_loss=stats[0])
