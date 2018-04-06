7d0e14ace96da0d574104dc3d8e9410d1bf4bec3
Connor <catalys1@gmail.com>
2018-04-06 11:24:00 -0600

    Update plot.py: allow multiple input files, flag to plot only validation set

--- plot.py
+++ plot.py
@@ -6,42 +6,51 @@ import os
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-f',
-    dest='fp',
+    'fp',
     metavar='FILE',
-    default='logs/stats',
+    nargs='+',
     help='Path to binary file containing data')
+parser.add_argument(
+    '-v', '--val-only',
+    dest='val_only',
+    action='store_true',
+    default=False,
+    help='Show only the validation loss/accuracy')
 args = parser.parse_args()
 
-fp = args.fp
-data = open(fp, 'rb').read()
-data = struct.unpack('f' * (len(data) // 4), data)
-data = np.array(data).reshape(-1, 4)
-t = np.linspace(1, data.shape[0] + 1, 4 * data.shape[0])
+files = args.fp
+val_only = args.val_only
+data = {}
+for f in files:
+    d = open(f, 'rb').read()
+    d = struct.unpack('f' * (len(d) // 4), d)
+    d = np.array(d).reshape(-1, 4)
+    data[f.split('/')[-1]] = d
 
 try:
-	import seaborn as sns
-	sns.set()
+    import seaborn as sns
+    sns.set()
+    sns.set_style('darkgrid', {'axes.facecolor':'.88'})
 except ImportError:
-	pass
-	
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-ax1.plot(data[:,0], color='b', label='train')
-ax1.plot(data[:,2], color='g', label='val')
-ax1.legend()
-ax1.set_title('Loss')
-ax1.set_ylabel('Loss (cross entropy)')
+    pass
 
-data[:,[1,3]] *= 100
-ax2.plot(data[:,1], color='b', label='train')
-ax2.plot(data[:,3], color='g', label='val')
-ax2.hlines(
-    (data[:,1].max(), data[:,3].max()),
-    0, len(data)-1,
-    colors=('b', 'g'),
-    linestyle='dotted')
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+for k, d in data.items():
+    d[:,[1,3]] *= 100
+
+    if not val_only:
+        ax1.plot(d[:,0], label=k+'-train')
+        ax2.plot(d[:,1], label=k+'-train')
+
+    ax1.plot(d[:,2], label=k+'-val')
+    ax2.plot(d[:,3], label=k+'-val')
+#   ax2.hlines(
+#       (d[:,1].max(), d[:,3].max()),
+#       0, len(d)-1,
+#       linestyle='dotted')
+ax1.legend()
+ax1.set_ylabel('Loss (cross entropy)')
 ax2.legend(loc='lower right')
-ax2.set_title('Accuracy')
 ax2.set_xlabel('Iterations')
 ax2.set_ylabel('Accuracy')
 
