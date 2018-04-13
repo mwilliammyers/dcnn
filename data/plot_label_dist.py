992492ad3a54340aa2d5b2fb355e908c9c9f33c8
William Myers <mwilliammyers@gmail.com>
2018-04-13 13:59:34 -0600

    Refactor plot_label_dist.py to accept cli

--- data/plot_label_dist.py
+++ data/plot_label_dist.py
@@ -1,27 +1,27 @@
+import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas
 
+for i, (file_path, lab) in enumerate(zip(sys.argv[1::2], sys.argv[2::2])):
+    data = pandas.read_csv(file_path)
 
-data = pandas.read_csv('twitter_airlines.csv')
-lab = 'airline_sentiment'
+    sns.set_style('darkgrid')
+    sns.set_context('paper')
 
-sns.set_style('darkgrid')
-sns.set_context('paper')
+    fig = plt.figure(i, (3, 2))
+    ax = fig.add_subplot(111)
 
-fig = plt.figure(1, (3,2))
-ax = fig.add_subplot(111)
-
-b = sns.countplot(x=lab, data=data, order=data[lab].value_counts().index, ax=ax)
-b.set_xlabel('')
-fig.tight_layout()
-for p, label in zip(b.patches, data[lab].value_counts().index):
-    b.annotate(
-        f'{data[lab].value_counts()[label]/len(data)*100:.0f}%', 
-        (p.get_x()+p.get_width()/2, p.get_y()+p.get_height()/2),
-        fontsize=10,
-        color='white',
-        ha='center',
-    )
+    b = sns.countplot(x=lab, data=data, order=data[lab].value_counts().index, ax=ax)
+    b.set_xlabel('')
+    fig.tight_layout()
+    for p, label in zip(b.patches, data[lab].value_counts().index):
+        b.annotate(
+            f'{data[lab].value_counts()[label]/len(data)*100:.0f}%',
+            (p.get_x() + p.get_width() / 2, p.get_y() + p.get_height() / 2),
+            fontsize=10,
+            color='white',
+            ha='center',
+        )
 
 plt.show()
