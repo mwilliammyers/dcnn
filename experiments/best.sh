00ea21061485cd2c125eef4e7a49ca5171aa74dd
William Myers <mwilliammyers@gmail.com>
2018-04-15 20:59:28 -0600

    Add support for twitter-large dataset in best.sh

--- experiments/best.sh
+++ experiments/best.sh
@@ -6,14 +6,14 @@ num_runs=3
 optim=adagrad
 
 # twitter uses defaults from paper and/or https://github.com/FredericGodin/DynamicCNN
-[[ $dataset = "twitter" ]] && num_epochs=5 || num_epochs=15
+[[ $dataset =~ twitter.* ]] && num_epochs=5 || num_epochs=15
 # our results seem to indicate 32 is also good for twitter
-[[ $dataset = "twitter" ]] && batch_size=4 || batch_size=128
+[[ $dataset =~ twitter.* ]] && batch_size=4 || batch_size=128
 # our results seem to indicate .05 is also good for twitter
-[[ $dataset = "twitter" ]] && lr=.1 || lr=.08
+[[ $dataset =~ twitter.* ]] && lr=.1 || lr=.08
 # our results seem to indicate 80 is also good for twitter
-[[ $dataset = "twitter" ]] && embed_dim=48 || embed_dim=200
-[[ $dataset = "twitter" ]] && eval_period=300 || eval_period=400
+[[ $dataset =~ twitter.* ]] && embed_dim=48 || embed_dim=200
+[[ $dataset =~ twitter.* ]] && eval_period=300 || eval_period=400
 
 for i in $(seq 1 $num_runs)
 do
