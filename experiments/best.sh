f6fb6b78fe15b7d6a25ef2eea0ac6cc74eb9ef1d
William Myers <mwilliammyers@gmail.com>
2018-04-15 20:55:54 -0600

    Increase eval period

--- experiments/best.sh
+++ experiments/best.sh
@@ -13,7 +13,7 @@ optim=adagrad
 [[ $dataset = "twitter" ]] && lr=.1 || lr=.08
 # our results seem to indicate 80 is also good for twitter
 [[ $dataset = "twitter" ]] && embed_dim=48 || embed_dim=200
-[[ $dataset = "twitter" ]] && eval_period=100 || eval_period=200
+[[ $dataset = "twitter" ]] && eval_period=300 || eval_period=400
 
 for i in $(seq 1 $num_runs)
 do
