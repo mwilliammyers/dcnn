ebbe4b370749cefaf3896309051d7bbeb8be0f37
William Myers <mwilliammyers@gmail.com>
2018-04-15 21:10:14 -0600

    Increase embedding dimension to 60

--- experiments/best.sh
+++ experiments/best.sh
@@ -12,7 +12,7 @@ optim=adagrad
 # our results seem to indicate .05 is also good for twitter
 [[ $dataset =~ twitter.* ]] && lr=.1 || lr=.08
 # our results seem to indicate 80 is also good for twitter
-[[ $dataset =~ twitter.* ]] && embed_dim=48 || embed_dim=200
+[[ $dataset =~ twitter.* ]] && embed_dim=60 || embed_dim=200
 [[ $dataset =~ twitter.* ]] && eval_period=300 || eval_period=400
 
 for i in $(seq 1 $num_runs)
