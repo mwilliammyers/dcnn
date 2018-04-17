8cae1c25b65b8fe2d50612b8571ec2baefe49d3c
William Myers <mwilliammyers@gmail.com>
2018-04-16 23:50:38 -0600

    Tweak hyperparams in best.sh

--- experiments/best.sh
+++ experiments/best.sh
@@ -7,12 +7,12 @@ optim=adagrad
 
 # twitter uses defaults from paper and/or https://github.com/FredericGodin/DynamicCNN
 [[ $dataset =~ twitter.* ]] && num_epochs=8 || num_epochs=10
-# our results seem to indicate 32 is also good for twitter; paper uses 4
-[[ $dataset =~ twitter.* ]] && batch_size=32 || batch_size=64
-# our results seem to indicate .05 is also good for twitter; paper uses .1
-[[ $dataset =~ twitter.* ]] && lr=.05 || lr=.02
+# our results seem to indicate 16 is also good for twitter; paper uses 4
+[[ $dataset =~ twitter.* ]] && batch_size=16 || batch_size=64
+# our results seem to indicate .03 is also good for twitter; paper uses .1
+[[ $dataset =~ twitter.* ]] && lr=.03 || lr=.02
 # our results seem to indicate 80 is also good for twitter; paper uses 60
-[[ $dataset =~ twitter.* ]] && embed_dim=80 || embed_dim=120
+[[ $dataset =~ twitter.* ]] && embed_dim=80 || embed_dim=180
 [[ $dataset =~ twitter.* ]] && eval_period=300 || eval_period=400
 
 for i in $(seq 1 $num_runs)
