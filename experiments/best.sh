2648ee84f91484e265836c4bdcc975e637610f27
William Myers <mwilliammyers@gmail.com>
2018-04-17 10:47:57 -0600

    Update best.sh

--- experiments/best.sh
+++ experiments/best.sh
@@ -7,15 +7,15 @@ optim=adagrad
 
 # twitter uses defaults from paper and/or https://github.com/FredericGodin/DynamicCNN
 [[ $dataset =~ twitter.* ]] && num_epochs=8 || num_epochs=10
-# our results seem to indicate 16 is also good for twitter; paper uses 4
+# we use 16; paper uses 4
 [[ $dataset =~ twitter.* ]] && batch_size=16 || batch_size=64
-# our results seem to indicate .03 is also good for twitter; paper uses .1
+# we use .03; paper uses .1
 [[ $dataset =~ twitter.* ]] && lr=.03 || lr=.02
-# our results seem to indicate 80 is also good for twitter; paper uses 60
-[[ $dataset =~ twitter.* ]] && embed_dim=80 || embed_dim=256
-[[ $dataset =~ twitter.* ]] && eval_period=200 || eval_period=200
-[[ $dataset =~ twitter.* ]] && kernel_sizes="7 5" || kernel_sizes="7 7 5 5 3 3"
-[[ $dataset =~ twitter.* ]] && num_filters="6 14" || num_filters="10 14 18 22 26 30"
+[[ $dataset =~ twitter.* ]] && eval_period=200 || eval_period=400
+# we use 80; paper uses 60; must be: k * 2**num_layers, k = an int multiple of 2
+[[ $dataset =~ twitter.* ]] && embed_dim=80 || embed_dim=128
+[[ $dataset =~ twitter.* ]] && kernel_sizes="7 5" || kernel_sizes="11 9 7 5 3"
+[[ $dataset =~ twitter.* ]] && num_filters="6 14" || num_filters="10 14 18 22 26"
 
 for i in $(seq 1 $num_runs)
 do
