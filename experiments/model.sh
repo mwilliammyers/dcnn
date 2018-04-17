1f2b06cb4eaa12ddfe29e6aa9083bdc780b3328d
William Myers <mwilliammyers@gmail.com>
2018-04-17 00:41:22 -0600

    Remove custom model support from model.sh

--- experiments/model.sh
+++ experiments/model.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 dataset="${1:-twitter}"
-models=(dcnn dcnn-custom mlp)
+models=(dcnn mlp)
 
 for model in "${models[@]}"
 do
-    [[ $model = "dcnn-custom" ]] && export CUDA_VISIBLE_DEVICES=-1 || export CUDA_VISIBLE_DEVICES=0
-	python3 dcnn.py --model $model --num-epochs 20 --batch-size 256 --dataset $dataset --log "logs/$dataset/model-$model"
+  python3 dcnn.py --model $model --dataset $dataset --log "logs/$dataset/model-$model"
 done
