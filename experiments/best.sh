cc6e0c67728a9de9416dfa012fc0704c7fd08566
William Myers <mwilliammyers@gmail.com>
2018-04-16 21:22:14 -0600

    Update best.sh

--- experiments/best.sh
+++ experiments/best.sh
@@ -6,13 +6,13 @@ num_runs=1
 optim=adagrad
 
 # twitter uses defaults from paper and/or https://github.com/FredericGodin/DynamicCNN
-[[ $dataset =~ twitter.* ]] && num_epochs=8 || num_epochs=15
+[[ $dataset =~ twitter.* ]] && num_epochs=8 || num_epochs=10
 # our results seem to indicate 32 is also good for twitter; paper uses 4
-[[ $dataset =~ twitter.* ]] && batch_size=32 || batch_size=128
+[[ $dataset =~ twitter.* ]] && batch_size=32 || batch_size=64
 # our results seem to indicate .05 is also good for twitter; paper uses .1
-[[ $dataset =~ twitter.* ]] && lr=.05 || lr=.04
+[[ $dataset =~ twitter.* ]] && lr=.05 || lr=.02
 # our results seem to indicate 80 is also good for twitter; paper uses 60
-[[ $dataset =~ twitter.* ]] && embed_dim=80 || embed_dim=100
+[[ $dataset =~ twitter.* ]] && embed_dim=80 || embed_dim=120
 [[ $dataset =~ twitter.* ]] && eval_period=300 || eval_period=400
 
 for i in $(seq 1 $num_runs)
